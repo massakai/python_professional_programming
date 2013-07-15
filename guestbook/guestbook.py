12646fee0f7367755b9ec77b7ef1f5a3e1d1f139
Masashi SAKAI <masashi.sakai1986@gmail.com>
2013-07-15 20:43:47 +0900

    use Flask

--- guestbook/guestbook.py
+++ guestbook/guestbook.py
@@ -1,5 +1,9 @@
 # coding: utf-8
 import shelve
+from datetime import datetime
+from flask import Flask, request, render_template, redirect, escape, Markup
+
+application = Flask(__name__)
 
 DATA_FILE = 'guestbook.dat'
 
@@ -35,3 +39,40 @@ def load_data():
     database.close()
     return greeting_list
 
+@application.route('/')
+def index():
+    """ トップページ
+    テンプレートを使用してページを表示します
+    """
+    # 投稿データを読み込みます
+    greeting_list = load_data()
+    return render_template('index.html', greeting_list=greeting_list)
+
+@application.route('/post', methods=['POST'])
+def post():
+    """ 投稿用URL
+    """
+    # 投稿されたデータを取得します
+    name = request.form.get('name') # 名前
+    comment = request.form.get('comment') # コメント
+    create_at = datetime.now() # 投稿日時（現在時間）
+    # データを保存します
+    save_data(name, comment, create_at)
+    # 保存後はトップページにリダイレクトします
+    return redirect('/')
+
+@application.template_filter('nl2br')
+def nl2br_filter(s):
+    """ 改行文字をbrタグに置き換えるテンプレートフィルタ
+    """
+    return escape(s).replace('\n', Markup('<br />'))
+
+@application.template_filter('datetime_fmt')
+def datatime_fmt_filter(dt):
+    """ datettimeオブジェクトを見やすい表示にするテンプレートフィルタ
+    """
+    return dt.strftime('%Y/%m/%d %H:%M:%S')
+
+if __name__ == '__main__':
+    # IPアドレス127.0.0.1の5000番ポートでアプリケーションを実行します
+    application.run('127.0.0.1', 5000, debug=True)
